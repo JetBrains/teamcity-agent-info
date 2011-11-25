669c2311b283deb01afcc5f6d57bacfe2087b561
Eugene.Petrenko <Eugene.Petrenko@fc027d59-cf41-0410-a13e-92ad4256a95e>
2011-11-25 15:22:22 +0000

    report free space
    git-svn-id: http://svn.jetbrains.org/teamcity/plugins/agent-info/trunk@4757 fc027d59-cf41-0410-a13e-92ad4256a95e

--- src/jetbrains/buildserver/agentInfo/AgentInfo.java
+++ src/jetbrains/buildserver/agentInfo/AgentInfo.java
@@ -2,44 +2,80 @@ package jetbrains.buildserver.agentInfo;
 
 import jetbrains.buildServer.agent.*;
 import jetbrains.buildServer.util.EventDispatcher;
+import jetbrains.buildServer.util.ExceptionUtil;
+import jetbrains.buildServer.util.FileUtil;
+import jetbrains.buildServer.util.NamedDeamonThreadFactory;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Yegor.Yarko
  *         Date: 10.02.2009
  */
 public class AgentInfo extends AgentLifeCycleAdapter {
-  long myPhysicalMemoryInMb = -1;
-  private int myAvailableProcessors = -1;
+  @NotNull private final BuildAgentConfiguration myConfig;
 
-  public AgentInfo(@NotNull final EventDispatcher<AgentLifeCycleListener> disp) {
-    disp.addListener(this);
+  public AgentInfo(@NotNull final BuildAgentConfiguration config,
+                   @NotNull final BuildAgent agent,
+                   @NotNull final EventDispatcher<AgentLifeCycleListener> events) {
+    myConfig = config;
+    final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor(new NamedDeamonThreadFactory("agent-info recent updates pool"));
+
+    final Runnable action = ExceptionUtil.catchAll("update free space", new Runnable() {
+      public void run() {
+        if (agent.isRunning()) return;
+
+        publishFreeSpace();
+      }
+    });
+
+    events.addListener(new AgentLifeCycleAdapter(){
+      @Override
+      public void pluginsLoaded() {
+        publishStaticParameters();
+
+        service.scheduleWithFixedDelay(action, 0, 5 * 60, TimeUnit.SECONDS);
+      }
+
+      @Override
+      public void runnerFinished(@NotNull BuildRunnerContext runner, @NotNull BuildFinishedStatus status) {
+        publishFreeSpace();
+      }
+
+      @Override
+      public void agentShutdown() {
+        service.shutdown();
+      }
+    });
   }
   
-  @Override
-  public void beforeAgentConfigurationLoaded(@NotNull BuildAgent agent) {
-    final BuildAgentConfiguration configuration = agent.getConfiguration();
-
-    init();
-
-    if (myPhysicalMemoryInMb > 0){
-      configuration.addCustomProperty(Constants.SYSTEM_PREFIX + "teamcity.agent.hardware.memorySize", (new Long(myPhysicalMemoryInMb)).toString());
-    }
-    if (myAvailableProcessors > 0){
-      configuration.addCustomProperty(Constants.SYSTEM_PREFIX + "teamcity.agent.hardware.cpuCount", (new Long(myAvailableProcessors)).toString());
-    }
-  }
-
-  private void init() {
+  private void publishStaticParameters() {
     final OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
     if (operatingSystemMXBean instanceof com.sun.management.OperatingSystemMXBean) {
       com.sun.management.OperatingSystemMXBean sunBean = ((com.sun.management.OperatingSystemMXBean)operatingSystemMXBean);
-      myPhysicalMemoryInMb = sunBean.getTotalPhysicalMemorySize() / (1024L * 1024L);
+      long myPhysicalMemoryInMb = sunBean.getTotalPhysicalMemorySize() / (1024L * 1024L);
+      if (myPhysicalMemoryInMb > 0){
+        myConfig.addConfigurationParameter("teamcity.agent.hardware.memorySize", String.valueOf(myPhysicalMemoryInMb));
+      }
     }
-    myAvailableProcessors = operatingSystemMXBean.getAvailableProcessors();
+
+    long myAvailableProcessors = operatingSystemMXBean.getAvailableProcessors();
+    if (myAvailableProcessors > 0){
+      myConfig.addConfigurationParameter("teamcity.agent.hardware.cpuCount", String.valueOf(myAvailableProcessors));
+    }
+  }
+
+
+  private void publishFreeSpace() {
+    final Long space = FileUtil.getFreeSpace(myConfig.getWorkDirectory());
+    if (space != null && space >= 0) {
+      myConfig.addConfigurationParameter("teamcity.agent.work.dir.freeSpaceBytes", space.toString());
+    }
   }
 
 }
